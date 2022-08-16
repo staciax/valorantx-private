9846b7145b9dc8f1c94b14414f666160f8c40cbe
xSTACIA <6505001948@rumail.ru.ac.th>
2022-08-17 00:42:30 +0700

    example updated

--- test.py
+++ test.py
@@ -7,22 +7,23 @@ async def main():
     async with client:
         await client.fetch_all_assets()
 
-        agent = client.asset.get_agent('killjoy')
+        agent = client.assets.get_agent('killjoy')
         print("Agent:", agent.name)
 
-        buddy = client.asset.get_buddy('rgx')
+        buddy = client.assets.get_buddy('rgx')
         print("Buddy:", buddy.name_localizations.japanese)
 
-        player_card = client.asset.get_player_card('The Way Forward Card')
+        player_card = client.assets.get_player_card('The Way Forward Card')
         print("Player Card:", player_card.name)
 
-        player_title = client.asset.get_player_title('Fortune Title')
+        player_title = client.assets.get_player_title('Fortune Title')
         print("Player Title:", player_title.name_localizations.german)
 
-        spray = client.asset.get_spray('20d547a4-4ec8-c9ef-dd9d-1c8b74d0e6f7')
+        spray = client.assets.get_spray('20d547a4-4ec8-c9ef-dd9d-1c8b74d0e6f7')
         print("Spray:", spray.icon)
 
-        bundle = client.asset.get_bundle('sentinels')
+        # TODO: object
+        bundle = client.assets.get_bundle('sentinels')
         print(bundle['displayName']['en-US'])
 
 if __name__ == '__main__':
