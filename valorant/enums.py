08a8c4d8d641e6673faaad6e7fefc451d558333e
xSTACIA <6505001948@rumail.ru.ac.th>
2022-08-23 18:44:44 +0700

    [Enum] add `LevelBorderID`
    - Renamed `EmptyTitle` to`EmptyTitleID`

--- valorant/enums.py
+++ valorant/enums.py
@@ -29,19 +29,7 @@ from __future__ import annotations
 
 import types
 from collections import namedtuple
-from typing import (
-    Any,
-    Dict,
-    ClassVar,
-    List,
-    Optional,
-    Mapping,
-    Iterator,
-    Type,
-    TypeVar,
-    Tuple,
-    TYPE_CHECKING
-)
+from typing import TYPE_CHECKING, Any, ClassVar, Dict, Iterator, List, Mapping, Optional, Tuple, Type, TypeVar, Final
 
 # -- https://github.com/Rapptz/discord.py/blob/master/discord/enums.py
 
@@ -170,8 +158,6 @@ else:
 
 # --
 
-EmptyTitle = 'd13e579c-435e-44d4-cec2-6eae5a3c5ed4'
-
 __all__ = (
     'Region',
     'Shard',
@@ -185,8 +171,11 @@ __all__ = (
     'Locale',
     'try_enum',
     'try_enum_key',
+    'LevelBorderID',
+    'EmptyTitleID'
 )
 
+EmptyTitleID = 'd13e579c-435e-44d4-cec2-6eae5a3c5ed4'  # TODO: Remove this when we have a proper title system
 
 class Region(Enum):
     NA = 'na'
@@ -235,7 +224,6 @@ class ItemType(Enum):
 
     def __str__(self) -> str:
         return self.value
-
 
 class AgentID(Enum):
     astra = '41fb69c1-4189-7b37-f117-bcaf1e96f1bf'
@@ -408,6 +396,34 @@ class Locale(Enum):
     def __str__(self) -> str:
         return self.value
 
+class LevelBorderID(Enum):
+
+    empty = '00000000-0000-0000-0000-000000000000'
+    _1 = 'ebc736cd-4b6a-137b-e2b0-1486e31312c9'
+    _20 = '5156a90d-4d65-58d0-f6a8-48a0c003878a'
+    _40 = '9c4afb15-40d7-3557-062a-4bb198cb9958'
+    _60 = 'e6238102-425c-a647-6685-e6af7f8982d9'
+    _80 = '49413ac2-4ed5-6953-5791-db838ccb58f3'
+    _100 = 'e05371e3-4ec4-a53e-168a-c49346a75c19'
+    _120 = '7e7feff1-44c2-301e-767d-d9b2b1cd9051'
+    _140 = '53d4ed03-4b29-5913-aeda-80a41afcef3a'
+    _160 = '6f610ab6-4a21-63fd-ac19-4a9204bc2721'
+    _180 = '547ac9dd-495d-f11d-d921-3fbd14604ae0'
+    _200 = 'bd1082ab-462c-3fb8-e049-28a9750acf0f'
+    _220 = '37a36996-41f3-6e26-c00b-46bf7c037482'
+    _240 = '5d0d6c6c-4f0a-dc65-e506-b786cc27dbe1'
+    _260 = '3635b061-4bf9-b937-55fe-44a4dd0ed3dc'
+    _280 = 'ae5eda0d-476b-a159-959c-df93374f4a69'
+    _300 = '3d90bc3a-4626-71d6-a17c-93ae14d05fb0'
+    _320 = '674bbd9e-4a4f-208a-75fa-1d9dd7d7008f'
+    _340 = 'd84cf377-4c21-1cdf-0260-4e8ebd9825f5'
+    _360 = '6c1fb61e-46e5-2908-5048-d4866cb64c3d'
+    _380 = 'af1852a5-4e66-02a6-2ae3-ab8c885efb80'
+    _400 = 'cbd1914e-43f8-7ae5-38c4-228bcbe58756'
+    _420 = 'c8a4abff-4ace-f0a3-c9f3-db936791a697'
+    _440 = '086dd1ab-4889-793a-4b33-0a99e311fa25'
+    _460 = '08ab72f1-4fce-ddb5-5fd5-22abd3bc9d49'
+    _480 = '6694d7f7-4ab9-8545-5921-35a9ea8cec24'
 
 class BundleID(Enum):
     Arcane = '2270b116-4255-8a14-4486-db9de4979b89'
