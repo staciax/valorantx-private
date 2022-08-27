6d707eaa3b8035fbcb6674dd5b1a342cadb563d6
xSTACIA <6505001948@rumail.ru.ac.th>
2022-08-27 23:36:45 +0700

    [localization] clean code and renamed

--- valorant/localization.py
+++ valorant/localization.py
@@ -39,12 +39,54 @@ class Localization:
         self.untranslated = untranslated
         self._locale = locale
 
+        # locale code
+        self.ar_AE: str = self.untranslated.get('ar-AE', self.default)
+        self.de_DE: str = self.untranslated.get('de-DE', self.default)
+        self.en_US: str = self.untranslated.get('en-US', self.default)
+        self.es_ES: str = self.untranslated.get('es-ES', self.default)
+        self.es_MX: str = self.untranslated.get('es-MX', self.default)
+        self.fr_FR: str = self.untranslated.get('fr-FR', self.default)
+        self.id_ID: str = self.untranslated.get('id-ID', self.default)
+        self.it_IT: str = self.untranslated.get('it-IT', self.default)
+        self.ja_JP: str = self.untranslated.get('ja-JP', self.default)
+        self.ko_KR: str = self.untranslated.get('ko-KR', self.default)
+        self.pl_PL: str = self.untranslated.get('pl-PL', self.default)
+        self.pt_BR: str = self.untranslated.get('pt-BR', self.default)
+        self.ru_RU: str = self.untranslated.get('ru-RU', self.default)
+        self.th_TH: str = self.untranslated.get('th-TH', self.default)
+        self.tr_TR: str = self.untranslated.get('tr-TR', self.default)
+        self.vi_VN: str = self.untranslated.get('vi-VN', self.default)
+        self.zh_CN: str = self.untranslated.get('zh-CN', self.default)
+        self.zh_TW: str = self.untranslated.get('zh-TW', self.default)
+
+        # locale language
+        self.arabic: str = self.ar_AE
+        self.german: str = self.de_DE
+        self.english: str = self.en_US
+        self.american_english: str = self.en_US
+        self.british_english: str = self.en_US
+        self.spanish: str = self.es_ES
+        self.spanish_mexican: str = self.es_MX
+        self.french: str = self.fr_FR
+        self.indonesian: str = self.id_ID
+        self.italian: str = self.it_IT
+        self.japanese: str = self.ja_JP
+        self.korean: str = self.ko_KR
+        self.polish: str = self.pl_PL
+        self.portuguese_brazil: str = self.pt_BR
+        self.russian: str = self.ru_RU
+        self.thai: str = self.th_TH
+        self.turkish: str = self.tr_TR
+        self.vietnamese: str = self.vi_VN
+        self.chinese_simplified: str = self.zh_CN
+        self.chinese_traditional: str = self.zh_TW
+
     def __repr__(self) -> str:
         return f'<Translator untranslated={self.untranslated!r}>'
 
     def __str__(self) -> str:
         """Returns the default locale."""
-        return self.english
+        return self.locale
 
     def __eq__(self, other: object) -> bool:
         return isinstance(other, Localization) and self.untranslated == other.untranslated
@@ -56,143 +98,20 @@ class Localization:
         return hash(self.untranslated)
 
     @property
-    def arabic(self) -> str:
-        """:class:`str`: Returns the arabic locale."""
-        return self.untranslated['ar-AE']
-
-    @property
-    def german(self) -> str:
-        """:class:`str`: Returns the german locale."""
-        return self.untranslated['de-DE']
-
-    @property
-    def english(self) -> str:
-        """:class:`str`: Returns the english locale."""
-        return self.untranslated['en-US']
-
-    @property
-    def american_english(self) -> str:
-        """:class:`str`: Returns the american english locale."""
-        return self.untranslated['en-US']
-
-    @property
-    def british_english(self) -> str:
-        """:class:`str`: Returns the british english locale."""
-        return self.untranslated['en-US']
-
-    @property
-    def spanish(self) -> str:
-        """:class:`str`: Returns the spanish locale."""
-        return self.untranslated['es-ES']
-
-    @property
-    def spanish_mexico(self) -> str:
-        """:class:`str`: Returns the spanish mexico locale."""
-        return self.untranslated['es-MX']
-
-    @property
-    def french(self) -> str:
-        """:class:`str`: Returns the french locale."""
-        return self.untranslated['fr-FR']
-
-    @property
-    def indonesian(self) -> str:
-        """:class:`str`: Returns the indonesian locale."""
-        return self.untranslated['id-ID']
-
-    @property
-    def italian(self) -> str:
-        """:class:`str`: Returns the italian locale."""
-        return self.untranslated['it-IT']
-
-    @property
-    def japanese(self) -> str:
-        """:class:`str`: Returns the japanese locale."""
-        return self.untranslated['ja-JP']
-
-    @property
-    def korean(self) -> str:
-        """:class:`str`: Returns the korean locale."""
-        return self.untranslated['ko-KR']
-
-    @property
-    def polish(self) -> str:
-        """:class:`str`: Returns the polish locale."""
-        return self.untranslated['pl-PL']
-
-    @property
-    def portuguese_brazil(self) -> str:
-        """:class:`str`: Returns the portuguese brazil locale."""
-        return self.untranslated['pt-BR']
-
-    @property
-    def russian(self) -> str:
-        """:class:`str`: Returns the russian locale."""
-        return self.untranslated['ru-RU']
-
-    @property
-    def thai(self) -> str:
-        """:class:`str`: Returns the thai locale."""
-        return self.untranslated['th-TH']
-
-    @property
-    def turkish(self) -> str:
-        """:class:`str`: Returns the turkish locale."""
-        return self.untranslated['tr-TR']
-
-    @property
-    def vietnamese(self) -> str:
-        """:class:`str`: Returns the vietnamese locale."""
-        return self.untranslated['vi-VN']
-
-    @property
-    def chinese(self) -> str:
-        """:class:`str`: Returns the chinese locale."""
-        return self.untranslated['zh-CN']
-
-    @property
-    def chinese_traditional(self) -> str:
-        """:class:`str`: Returns the chinese traditional locale."""
-        return self.untranslated['zh-TW']
-
-    @property
     def default(self) -> str:
         """:class:`str`: Returns the english locale is default."""
-        return self.english
+        if isinstance(self.untranslated, str):
+            return self.untranslated
+        return self.untranslated.get('en-US')
 
     @property
     def locale(self) -> str:
         """:class:`str`: Returns from your current locale."""
-        return self._locale
+        return self.untranslated.get(str(self._locale), self.default)
 
-    def from_str(self, value: str):
-        """Constructs a :class:`Translator` from a string."""
+    def from_locale_code(self, value: str):
+        """:class:`str`: Returns the locale from the locale code."""
         locale = getattr(self, value.lower())
         if locale is None:
             raise ValueError("Unknown locale.")
         return locale
-
-    def to_dict(self) -> Dict[str, str]:  # TODO: payload hint
-        result = {
-            'arabic': self.arabic,
-            'german': self.german,
-            'english': self.english,
-            'american_english': self.american_english,
-            'british_english': self.british_english,
-            'spanish': self.spanish,
-            'spanish_mexico': self.spanish_mexico,
-            'french': self.french,
-            'indonesian': self.indonesian,
-            'italian': self.italian,
-            'japanese': self.japanese,
-            'korean': self.korean,
-            'polish': self.polish,
-            'portuguese_brazil': self.portuguese_brazil,
-            'russian': self.russian,
-            'thai': self.thai,
-            'turkish': self.turkish,
-            'vietnamese': self.vietnamese,
-            'chinese': self.chinese,
-            'chinese_traditional': self.chinese_traditional,
-        }
-        return result
