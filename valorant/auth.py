5737439ed503055793ca065724bad1665fc3ad2f
xSTACIA <6505001948@rumail.ru.ac.th>
2022-09-27 19:44:02 +0700

    [auth] use auth from floxay

--- valorant/auth.py
+++ valorant/auth.py
@@ -26,17 +26,11 @@ Links to the original source code: https://github.com/floxay/python-riot-auth
 
 """
 
-import ctypes
-import json
-import ssl
-import sys
-import warnings
-from base64 import urlsafe_b64decode
 from secrets import token_urlsafe
-from typing import Any, Dict, List, Optional, Sequence, Tuple, Union
-from urllib.parse import parse_qsl, urlsplit
+from typing import Any, Dict, Optional
 
 import aiohttp
+from riot_auth import RiotAuth as _RiotAuth
 
 from .errors import (
     RiotAuthenticationError,
@@ -64,48 +58,7 @@ class _CookieSentinel:
 _cookie_jar: Any = _CookieSentinel()
 
 
-class RiotAuth:
-    RIOT_CLIENT_USER_AGENT = "RiotClient/56.0.0.4578455.4552318 %s (Windows;10;;Professional, x64)"
-    CIPHERS13 = ":".join(  # https://docs.python.org/3/library/ssl.html#tls-1-3
-        (
-            "TLS_CHACHA20_POLY1305_SHA256",
-            "TLS_AES_128_GCM_SHA256",
-            "TLS_AES_256_GCM_SHA384",
-        )
-    )
-    CIPHERS = ":".join(
-        (
-            "ECDHE-ECDSA-CHACHA20-POLY1305",
-            "ECDHE-RSA-CHACHA20-POLY1305",
-            "ECDHE-ECDSA-AES128-GCM-SHA256",
-            "ECDHE-RSA-AES128-GCM-SHA256",
-            "ECDHE-ECDSA-AES256-GCM-SHA384",
-            "ECDHE-RSA-AES256-GCM-SHA384",
-            "ECDHE-ECDSA-AES128-SHA",
-            "ECDHE-RSA-AES128-SHA",
-            "ECDHE-ECDSA-AES256-SHA",
-            "ECDHE-RSA-AES256-SHA",
-            "AES128-GCM-SHA256",
-            "AES256-GCM-SHA384",
-            "AES128-SHA",
-            "AES256-SHA",
-            "DES-CBC3-SHA",  # most likely not available
-        )
-    )
-    SIGALGS = ":".join(
-        (
-            "ecdsa_secp256r1_sha256",
-            "rsa_pss_rsae_sha256",
-            "rsa_pkcs1_sha256",
-            "ecdsa_secp384r1_sha384",
-            "rsa_pss_rsae_sha384",
-            "rsa_pkcs1_sha384",
-            "rsa_pss_rsae_sha512",
-            "rsa_pkcs1_sha512",
-            "rsa_pkcs1_sha1",  # will get ignored and won't be negotiated
-        )
-    )
-
+class RiotAuth(_RiotAuth):
     def __init__(self) -> None:
         self._auth_ssl_ctx = RiotAuth.create_riot_auth_ssl_ctx()
         self._cookie_jar: aiohttp.CookieJar = _cookie_jar
@@ -136,68 +89,6 @@ class RiotAuth:
         if self.name is None or self.tag is None:
             return ''
         return f'{self.name}#{self.tag}'
-
-    @staticmethod
-    def create_riot_auth_ssl_ctx() -> ssl.SSLContext:
-        ssl_ctx = ssl.create_default_context()
-
-        # https://github.com/python/cpython/issues/88068
-        addr = id(ssl_ctx) + sys.getsizeof(object())
-        ssl_ctx_addr = ctypes.cast(addr, ctypes.POINTER(ctypes.c_void_p)).contents
-
-        if sys.platform.startswith("win32"):
-            libssl = ctypes.CDLL("libssl-1_1.dll")
-        elif sys.platform.startswith(("linux", "darwin")):
-            libssl = ctypes.CDLL(ssl._ssl.__file__)
-        else:
-            raise NotImplementedError("Only Windows (win32), Linux (linux) and macOS (darwin) are supported.")
-
-        with warnings.catch_warnings():
-            warnings.filterwarnings("ignore", category=DeprecationWarning)
-            ssl_ctx.minimum_version = ssl.TLSVersion.TLSv1  # deprecated since 3.10
-        ssl_ctx.set_alpn_protocols(["http/1.1"])
-        ssl_ctx.options |= 1 << 19  # SSL_OP_NO_ENCRYPT_THEN_MAC
-        libssl.SSL_CTX_set_ciphersuites(ssl_ctx_addr, RiotAuth.CIPHERS13.encode())
-        libssl.SSL_CTX_set_cipher_list(ssl_ctx_addr, RiotAuth.CIPHERS.encode())
-        # setting SSL_CTRL_SET_SIGALGS_LIST
-        libssl.SSL_CTX_ctrl(ssl_ctx_addr, 98, 0, RiotAuth.SIGALGS.encode())
-
-        # print([cipher["name"] for cipher in ssl_ctx.get_ciphers()])
-        return ssl_ctx
-
-    def __update(
-        self,
-        extract_jwt: bool = False,
-        key_attr_pairs: Sequence[Tuple[str, str]] = (
-            ("sub", "user_id"),
-            ("exp", "expires_at"),
-        ),
-        **kwargs,
-    ) -> None:
-        # ONLY PREDEFINED PUBLIC KEYS ARE SET, rest is silently ignored!
-        predefined_keys = [key for key in self.__dict__.keys() if key[0] != "_"]
-
-        self.__dict__.update((key, val) for key, val in kwargs.items() if key in predefined_keys)
-
-        if extract_jwt:  # extract additional data from access JWT
-            additional_data = self.__get_keys_from_access_token(key_attr_pairs)
-            self.__dict__.update((key, val) for key, val in additional_data if key in predefined_keys)
-
-    def __get_keys_from_access_token(
-        self, key_attr_pairs: Sequence[Tuple[str, str]]
-    ) -> List[Tuple[str, Union[str, int, List, Dict, None]]]:  # List[Tuple[str, JSONType]]
-        payload = self.access_token.split(".")[1]
-        decoded = urlsafe_b64decode(f"{payload}===")
-        temp_dict: Dict = json.loads(decoded)
-        return [(attr, temp_dict.get(key)) for key, attr in key_attr_pairs]
-
-    def __set_tokens_from_uri(self, data: Dict) -> None:
-        mode = data["response"]["mode"]
-        uri = data["response"]["parameters"]["uri"]
-
-        result = getattr(urlsplit(uri), mode)
-        data = dict(parse_qsl(result))
-        self.__update(extract_jwt=True, **data)
 
     async def authorize(
         self, username: str, password: str, use_query_response_mode: bool = False, remember: bool = False
