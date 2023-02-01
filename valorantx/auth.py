5064a75f4cea3015f5d18146197cebf9c9f89609
STACIA <69411527+staciax@users.noreply.github.com>
2023-02-01 14:35:46 +0700

    double quote to single quote

--- valorantx/auth.py
+++ valorantx/auth.py
@@ -42,7 +42,7 @@ from .errors import (
 
 # fmt: off
 __all__: Tuple[str, ...] = (
-    "RiotAuth",
+    'RiotAuth',
 )
 # fmt: on
 
@@ -105,86 +105,86 @@ class RiotAuth(_RiotAuth):
         conn = aiohttp.TCPConnector(ssl=self._auth_ssl_ctx)
         async with aiohttp.ClientSession(connector=conn, raise_for_status=True, cookie_jar=self._cookie_jar) as session:
             headers = {
-                "Accept-Encoding": "deflate, gzip, zstd",
-                "user-agent": RiotAuth.RIOT_CLIENT_USER_AGENT % "rso-auth",
-                "Cache-Control": "no-assets",
-                "Accept": "application/json",
+                'Accept-Encoding': 'deflate, gzip, zstd',
+                'user-agent': RiotAuth.RIOT_CLIENT_USER_AGENT % 'rso-auth',
+                'Cache-Control': 'no-assets',
+                'Accept': 'application/json',
             }
 
             # region Begin auth/Reauth
             body = {
-                "acr_values": "",
-                "claims": "",
-                "client_id": "riot-client",
-                "code_challenge": "",
-                "code_challenge_method": "",
-                "nonce": token_urlsafe(16),
-                "redirect_uri": "http://localhost/redirect",
-                "response_type": "token id_token",
-                "scope": "openid link ban lol_region account",
+                'acr_values': '',
+                'claims': '',
+                'client_id': 'riot-client',
+                'code_challenge': '',
+                'code_challenge_method': '',
+                'nonce': token_urlsafe(16),
+                'redirect_uri': 'http://localhost/redirect',
+                'response_type': 'token id_token',
+                'scope': 'openid link ban lol_region account',
             }
             if use_query_response_mode:
-                body["response_mode"] = "query"
+                body['response_mode'] = 'query'
             async with session.post(
-                "https://auth.riotgames.com/api/v1/authorization",
+                'https://auth.riotgames.com/api/v1/authorization',
                 json=body,
                 headers=headers,
             ) as r:
                 data: Dict = await r.json()
-                resp_type = data["type"]
+                resp_type = data['type']
             # endregion
 
-            if resp_type != "response":  # not reauth
+            if resp_type != 'response':  # not reauth
                 # region Authenticate
                 body = {
-                    "language": "en_US",
-                    "password": password,
-                    "region": None,
-                    "remember": remember,
-                    "type": "auth",
-                    "username": username,
+                    'language': 'en_US',
+                    'password': password,
+                    'region': None,
+                    'remember': remember,
+                    'type': 'auth',
+                    'username': username,
                 }
                 async with session.put(
-                    "https://auth.riotgames.com/api/v1/authorization",
+                    'https://auth.riotgames.com/api/v1/authorization',
                     json=body,
                     headers=headers,
                 ) as r:
                     data: Dict = await r.json()
-                    resp_type = data["type"]
-                    if resp_type == "response":
+                    resp_type = data['type']
+                    if resp_type == 'response':
                         ...
-                    elif resp_type == "auth":
-                        err = data.get("error")
-                        if err == "auth_failure":
+                    elif resp_type == 'auth':
+                        err = data.get('error')
+                        if err == 'auth_failure':
                             raise RiotAuthenticationError(
-                                f"Failed to authenticate. Make sure username and password are correct. `{err}`."
+                                f'Failed to authenticate. Make sure username and password are correct. `{err}`.'
                             )
-                        elif err == "rate_limited":
+                        elif err == 'rate_limited':
                             raise RiotRatelimitError()
                         else:
-                            raise RiotUnknownErrorTypeError(f"Got unknown error `{err}` during authentication.")
-                    elif resp_type == "multifactor":
+                            raise RiotUnknownErrorTypeError(f'Got unknown error `{err}` during authentication.')
+                    elif resp_type == 'multifactor':
                         if self.__waif_for_2fa:
                             if 'method' in data['multifactor']:
                                 if data['multifactor']['method'] == 'email':
                                     self.multi_factor_email = data['multifactor']['email']
-                        raise RiotMultifactorError("Multi-factor authentication is not currently supported.")
+                        raise RiotMultifactorError('Multi-factor authentication is not currently supported.')
                     else:
-                        raise RiotUnknownResponseTypeError(f"Got unknown response type `{resp_type}` during authentication.")
+                        raise RiotUnknownResponseTypeError(f'Got unknown response type `{resp_type}` during authentication.')
                 # endregion
 
-            self._cookie_jar = session.cookie_jar
+            self._cookie_jar = session.cookie_jar  # type: ignore
             self.__set_tokens_from_uri(data)
 
             # region Get new entitlements token
-            headers["Authorization"] = f"{self.token_type} {self.access_token}"
+            headers['Authorization'] = f'{self.token_type} {self.access_token}'
             async with session.post(
-                "https://entitlements.auth.riotgames.com/api/token/v1",
+                'https://entitlements.auth.riotgames.com/api/token/v1',
                 headers=headers,
                 json={},
-                # json={"urn": "urn:entitlement:%"},
+                # json={'urn': 'urn:entitlement:%'},
             ) as r:
-                self.entitlements_token = (await r.json())["entitlements_token"]
+                self.entitlements_token = (await r.json())['entitlements_token']
 
             # Get user info
 
@@ -196,7 +196,7 @@ class RiotAuth(_RiotAuth):
 
             # Get regions
 
-            body = {"id_token": self.id_token}
+            body = {'id_token': self.id_token}
             async with session.put(
                 'https://riot-geo.pas.si.riotgames.com/pas/v1/product/valorant', headers=headers, json=body
             ) as r:
@@ -211,7 +211,7 @@ class RiotAuth(_RiotAuth):
         Returns a ``bool`` indicating success or failure.
         """
         try:
-            await self.authorize("", "")
+            await self.authorize('', '')
             return True
         except RiotAuthenticationError:  # because credentials are empty
             return False
@@ -220,30 +220,30 @@ class RiotAuth(_RiotAuth):
         """
         Set the token data from a dictionary.
         """
-        self.access_token = data["access_token"]
-        self.id_token = data["id_token"]
-        self.entitlements_token = data["entitlements_token"]
-        self.token_type = data["token_type"]
-        self.expires_at = int(data["expires_at"])
-        self.user_id = data["user_id"]
-        self.puuid = data["puuid"]
-        self.name = data["name"]
-        self.tag = data["tag"]
-        self.region = data["region"]
+        self.access_token = data['access_token']
+        self.id_token = data['id_token']
+        self.entitlements_token = data['entitlements_token']
+        self.token_type = data['token_type']
+        self.expires_at = int(data['expires_at'])
+        self.user_id = data['user_id']
+        self.puuid = data['puuid']
+        self.name = data['name']
+        self.tag = data['tag']
+        self.region = data['region']
 
     def to_dict(self) -> Dict[str, Any]:
         """
         Return the token data as a dictionary.
         """
         return {
-            "access_token": self.access_token,
-            "id_token": self.id_token,
-            "entitlements_token": self.entitlements_token,
-            "token_type": self.token_type,
-            "expires_at": self.expires_at,
-            "user_id": self.user_id,
-            "puuid": self.puuid,
-            "name": self.name,
-            "tag": self.tag,
-            "region": self.region,
+            'access_token': self.access_token,
+            'id_token': self.id_token,
+            'entitlements_token': self.entitlements_token,
+            'token_type': self.token_type,
+            'expires_at': self.expires_at,
+            'user_id': self.user_id,
+            'puuid': self.puuid,
+            'name': self.name,
+            'tag': self.tag,
+            'region': self.region,
         }
