a47ed95e0717de4631e916e5adc7a8d882721af7
STACIA <69411527+staciax@users.noreply.github.com>
2023-03-29 22:30:03 +0700

    update test files

--- tests/test_assets.py
+++ tests/test_assets.py
@@ -1,6 +1,8 @@
 import asyncio
 import logging
 
+import pytest
+
 import valorantx
 
 client = valorantx.Client(locale=valorantx.Locale.japanese)
@@ -9,108 +11,134 @@ client = valorantx.Client(locale=valorantx.Locale.japanese)
 logging.basicConfig(level=logging.ERROR)
 
 
-async def main():
+@pytest.mark.asyncio
+async def test_assets():
     async with client:
-
         await client.fetch_assets(reload=True)
         # after fetch_assets, you can comment above line and use below line
         # client.reload_assets()
         # if new version available, please use `await client.fetch_assets()` again
 
+        # TODO: assert all
+
         agent = client.get_agent('Killjoy')
-        print(repr(agent))
+        assert agent is not None
+        assert agent.display_name.default == 'Killjoy'
 
         buddy = client.get_buddy({"ar-AE": "ملحق فرقة العمليات 809"})
-        print(repr(buddy))
+        assert buddy is not None
+        assert buddy.display_name.ar_AE == 'ملحق فرقة العمليات 809'
 
         bundle = client.get_bundle('Champions 2022')
-        print(repr(bundle))
+        assert bundle is not None
+        assert bundle.display_name.default == 'Champions 2022'
 
         player_card = client.get_player_card('The Way Forward Card')
-        print(repr(player_card))
+        assert player_card is not None
+        assert player_card.display_name.default == 'The Way Forward Card'
 
         player_title = client.get_player_title('Fortune Title')
-        print(repr(player_title))
+        assert player_title is not None
+        assert player_title.display_name.default == 'Fortune Title'
 
-        spray = client.get_spray('60adbf1b-48b7-b4a1-ccee-50b889c34514')
-        print(repr(spray))
+        spray = client.get_spray('3d2bcfc5-442b-812e-3c08-9180d6b36077')
+        assert spray is not None
+        assert spray.uuid == '3d2bcfc5-442b-812e-3c08-9180d6b36077'
+
+        spray_level = client.get_spray_level('60adbf1b-48b7-b4a1-ccee-50b889c34514')
+        assert spray_level is not None
+        assert spray_level.uuid == '60adbf1b-48b7-b4a1-ccee-50b889c34514'
 
         mission = client.get_mission('2bbe195c-48ce-ae72-2a0e-8288b8827f1f')
-        print(repr(mission))
+        assert mission is not None
+        assert mission.uuid == '2bbe195c-48ce-ae72-2a0e-8288b8827f1f'
 
         content_tier = client.get_content_tier('0cebb8be-46d7-c12a-d306-e9907bfc5a25')
-        print(repr(content_tier))
+        assert content_tier is not None
+        assert content_tier.uuid == '0cebb8be-46d7-c12a-d306-e9907bfc5a25'
 
         contract = client.get_contract('7ae5ad85-400b-beba-989d-42924ccf39be')
-        print(repr(contract))
+        assert contract is not None
+        assert contract.uuid == '7ae5ad85-400b-beba-989d-42924ccf39be'
 
         weapon = client.get_weapon('Operator')
-        print(repr(weapon))
+        assert weapon is not None
+        assert weapon.display_name.default == 'Operator'
 
         if weapon is not None:
             for skin in weapon.get_skins():
-                print(repr(skin))
+                assert skin is not None
                 for chroma in skin.chromas:
-                    print(repr(chroma))
+                    assert chroma is not None
                 for level in skin.levels:
-                    print(repr(level))
+                    assert level is not None
 
         skin = client.get_skin('Glitchpop Odin')
-        if skin is not None:
-            print(repr(skin.get_weapon()))
+        assert skin is not None
+        assert skin.display_name.default == 'Glitchpop Odin'
+        assert skin.get_weapon() is not None
 
         skin_level = client.get_skin_level('Xenohunter Odin Level 2')
-        if skin_level is not None:
-            print(repr(skin_level.get_weapon()))
-            print(repr(skin_level.get_skin()))
-
+        assert skin_level is not None
+        assert skin_level.display_name.default == 'Xenohunter Odin Level 2'
+        assert skin_level.get_weapon() is not None
+        assert skin_level.get_skin() is not None
 
         skin_chroma = client.get_skin_chroma('Glitchpop Odin')
-        if skin_chroma is not None:
-            print(repr(skin_chroma.get_weapon()))
-            print(repr(skin_chroma.get_skin()))
+        assert skin_chroma is not None
+        assert skin_chroma.display_name.default == 'Glitchpop Odin'
+        assert skin_chroma.get_weapon() is not None
+        assert skin_chroma.get_skin() is not None
 
         all_bundles = client.get_all_bundles()
         print(list(sorted(all_bundles, key=lambda b: b.display_name)))
 
         ceremony = client.get_ceremony('1e71c55c-476e-24ac-0687-e48b547dbb35')
-        print(repr(ceremony))
+        assert ceremony is not None
 
         competitive_tier = client.get_competitive_tier('564d8e28-c226-3180-6285-e48a390db8b1')
-        if competitive_tier is not None:
-            print(repr(competitive_tier.get_tiers()))
-            print(repr(competitive_tier))
+        assert competitive_tier is not None
+        assert competitive_tier.uuid == '564d8e28-c226-3180-6285-e48a390db8b1'
+        assert competitive_tier.get_tiers() is not None
 
         currency = client.get_currency('VP')
-        print(repr(currency))
+        assert currency is not None
+        assert currency.display_name.default == 'VP'
 
         game_mode = client.get_game_mode('96bd3920-4f36-d026-2b28-c683eb0bcac5')
-        print(repr(game_mode))
+        assert game_mode is not None
+        assert game_mode.uuid == '96bd3920-4f36-d026-2b28-c683eb0bcac5'
 
         game_mode_equippable = client.get_game_mode_equippable('c5de005c-4bdc-26a7-a47d-c295eaaae9d8')
-        print(repr(game_mode_equippable))
+        assert game_mode_equippable is not None
+        assert game_mode_equippable.uuid == 'c5de005c-4bdc-26a7-a47d-c295eaaae9d8'
 
         gear = client.get_gear('Light')
-        print(repr(gear))
+        assert gear is not None
+        assert gear.display_name.default == 'Light Shields'
 
         event = client.get_event('024d36a7-46e3-8a29-30c6-09a7fb81bebe')
-        print(repr(event))
+        assert event is not None
 
         level_border = client.get_level_border('ebc736cd-4b6a-137b-e2b0-1486e31312c9')
-        print(repr(level_border))
+        assert level_border is not None
+        assert level_border.uuid == 'ebc736cd-4b6a-137b-e2b0-1486e31312c9'
 
         level_border = client.get_level_border(20)
-        print(repr(level_border))
+        assert level_border is not None
+        assert level_border.starting_level >= 20
 
         map_ = client.get_map('ascent')
-        print(repr(map_))
+        assert map_ is not None
 
         season = client.get_season('0df5adb9-4dcb-6899-1306-3e9860661dd3')
-        print(repr(season))
+        assert season is not None
+        assert season.uuid == '0df5adb9-4dcb-6899-1306-3e9860661dd3'
 
         theme = client.get_theme('fdfe356c-40c4-ac6a-864e-16998fc784ef')
-        print(repr(theme))
+        assert theme is not None
+        assert theme.uuid == 'fdfe356c-40c4-ac6a-864e-16998fc784ef'
 
 
 if __name__ == '__main__':
-    asyncio.run(main())
+    asyncio.run(test_assets())
--- tests/test_in_game.py
+++ tests/test_in_game.py
@@ -1,5 +1,11 @@
 import asyncio
 import logging
+import os
+
+import pytest
+from dotenv import load_dotenv
+
+load_dotenv()
 
 import valorantx
 
@@ -9,9 +15,14 @@ logging.basicConfig(level=logging.ERROR)
 client = valorantx.Client(locale=valorantx.Locale.thai)
 
 
-async def main():
+@pytest.mark.asyncio
+async def test_in_game() -> None:
     async with client:
-        await client.authorize('username', 'password')
+        username = os.getenv('VALORANT_USERNAME')
+        assert username is not None
+        password = os.getenv('VALORANT_PASSWORD')
+        assert password is not None
+        await client.authorize(username, password)
 
         await client.fetch_assets(reload=True)  # 'with_price=True' is requires authorization
         # after `client.fetch_assets`, you can comment above line and use below line
@@ -19,11 +30,13 @@ async def main():
         # if new version available, please use `await client.fetch_assets(with_price=True)` again
 
         sf = await client.fetch_store_front()
+        assert sf is not None
+        assert sf.get_store() is not None
         for skin in sf.get_store():
-            print(skin.display_name)
-            print(skin.display_icon)
-            print(skin.display_name_localized.japanese)
-            print(skin.price)
+            assert skin is not None
+            assert skin.display_name is not None
+            assert skin.display_icon is not None
+            assert skin.price is not None
 
         history = await client.fetch_match_history(queue=valorantx.QueueType.deathmatch)
         print(repr(history))
@@ -46,4 +59,4 @@ async def main():
 
 
 if __name__ == '__main__':
-    asyncio.run(main())
+    asyncio.run(test_in_game())
