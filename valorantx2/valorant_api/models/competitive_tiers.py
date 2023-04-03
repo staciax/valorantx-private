0bab4ca306d5930db46f50d041f663ecbedba4d4
STACIA <69411527+staciax@users.noreply.github.com>
2023-04-03 19:26:57 +0700

    add models

---
+++ valorantx2/valorant_api/models/competitive_tiers.py
@@ -0,0 +1,138 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, Dict, List, Optional, Union
+
+from ...enums import Locale
+from .. import utils
+from ..asset import Asset
+from ..localization import Localization
+from .abc import BaseModel
+
+if TYPE_CHECKING:
+    # from typing_extensions import Self
+    from ..cache import CacheState
+    from ..types.competitive_tiers import CompetitiveTier as CompetitiveTierPayload, Tier as TierPayload
+
+# fmt: off
+__all__ = (
+    'CompetitiveTier',
+)
+# fmt: on
+
+
+class Tier:
+    def __init__(self, state: CacheState, data: TierPayload) -> None:
+        self._state: CacheState = state
+        self.tier: int = data['tier']
+        self._name: Union[str, Dict[str, str]] = data['tierName']
+        self._division: Optional[str] = data['division']
+        self._division_name: Union[str, Dict[str, str]] = data['divisionName']
+        self.color: str = data['color']
+        self.background_color: str = data['backgroundColor']
+        self._small_icon: Optional[str] = data['smallIcon']
+        self._large_icon: Optional[str] = data['largeIcon']
+        self._rank_triangle_down_icon: Optional[str] = data['rankTriangleDownIcon']
+        self._rank_triangle_up_icon: Optional[str] = data['rankTriangleUpIcon']
+        self._name_locale: Localization = Localization(self._name, locale=self._state.locale)
+        self._division_name_localized: Localization = Localization(self._division_name, locale=self._state.locale)
+
+    def __str__(self) -> str:
+        return self.display_name.locale
+
+    def __repr__(self) -> str:
+        return f'<Tier tier={self.tier!r} display_name={self.display_name!r} division={self.division!r}>'
+
+    def __hash__(self) -> int:
+        return hash(self.tier)
+
+    def __eq__(self, other) -> bool:
+        return isinstance(other, Tier) and self.tier == other.tier
+
+    def __ne__(self, other) -> bool:
+        return not self.__eq__(other)
+
+    def __lt__(self, other: object) -> bool:
+        return isinstance(other, Tier) and self.tier < other.tier
+
+    def __le__(self, other: object) -> bool:
+        return isinstance(other, Tier) and self.tier <= other.tier
+
+    def __gt__(self, other: object) -> bool:
+        return isinstance(other, Tier) and self.tier > other.tier
+
+    def __ge__(self, other: object) -> bool:
+        return isinstance(other, Tier) and self.tier >= other.tier
+
+    def display_name_localized(self, locale: Optional[Union[Locale, str]] = None) -> str:
+        return self._name_locale.from_locale(locale)
+
+    def division_name_localized(self, locale: Optional[Union[Locale, str]] = None) -> str:
+        return self._division_name_localized.from_locale(locale)
+
+    @property
+    def display_name(self) -> Localization:
+        """:class: `str` Returns the tier's name."""
+        return self._name_locale
+
+    @property
+    def division(self) -> Optional[str]:
+        """:class: `str` Returns the tier's division."""
+        return utils.removeprefix(self._division, 'ECompetitiveDivision::') if self._division else None
+
+    @property
+    def division_name(self) -> Localization:
+        """:class: `str` Returns the tier's division."""
+        return self._division_name_localized
+
+    @property
+    def small_icon(self) -> Optional[Asset]:
+        """:class: `Asset` Returns the tier's small icon."""
+        if self._small_icon is None:
+            return None
+        return Asset(self._state, self._small_icon)
+
+    @property
+    def large_icon(self) -> Optional[Asset]:
+        """:class: `Asset` Returns the tier's large icon."""
+        if self._large_icon is None:
+            return None
+        return Asset(self._state, self._large_icon)
+
+    @property
+    def rank_triangle_down_icon(self) -> Optional[Asset]:
+        """:class: `Asset` Returns the tier's rank triangle down icon."""
+        if self._rank_triangle_down_icon is None:
+            return None
+        return Asset(self._state, self._rank_triangle_down_icon)
+
+    @property
+    def rank_triangle_up_icon(self) -> Optional[Asset]:
+        """:class: `Asset` Returns the tier's rank triangle up icon."""
+        if self._rank_triangle_up_icon is None:
+            return None
+        return Asset(self._state, self._rank_triangle_up_icon)
+
+
+class CompetitiveTier(BaseModel):
+    def __init__(self, state: CacheState, data: CompetitiveTierPayload) -> None:
+        super().__init__(data['uuid'])
+        self._state: CacheState = state
+        self.asset_object_name: str = data['assetObjectName']
+        self._tiers: List[TierPayload] = data['tiers']
+        self.asset_path: str = data['assetPath']
+
+    def __str__(self) -> str:
+        return self.asset_object_name
+
+    def __repr__(self) -> str:
+        return f'<CompetitiveTier asset_object_name={self.asset_object_name!r}>'
+
+    def get_tiers(self) -> List[Tier]:
+        """:class: `list` Returns the competitive tier's tiers."""
+        return [Tier(state=self._state, data=tier) for tier in self._tiers]
+
+    # @classmethod
+    # def _from_uuid(cls, client: Client, uuid: str) -> Optional[Self]:
+    #     """Returns the competitive tier with the given UUID."""
+    #     data = client._assets.get_competitive_tier(uuid)
+    #     return cls(self._state, data=data) if data else None
---
+++ valorantx2/valorant_api/models/missions.py
@@ -0,0 +1,86 @@
+from __future__ import annotations
+
+import datetime
+from typing import TYPE_CHECKING, Dict, List, Optional, Union
+
+from ...enums import Locale, MissionType, try_enum
+from .. import utils
+from ..localization import Localization
+from .abc import BaseModel
+
+if TYPE_CHECKING:
+    # from typing_extensions import Self
+    from ..cache import CacheState
+    from ..types.missions import Mission as MissionPayload, Objective as ObjectivePayload
+
+# fmt: off
+__all__ = (
+    'Mission',
+)
+# fmt: on
+
+
+class Mission(BaseModel):
+    def __init__(self, state: CacheState, data: MissionPayload) -> None:
+        super().__init__(data['uuid'])
+        self._state: CacheState = state
+        self._display_name: Optional[Union[str, Dict[str, str]]] = data['displayName']
+        self._title: Optional[Union[str, Dict[str, str]]] = data['title']
+        self._type: Optional[str] = data.get('type')
+        self.xp: int = data['xpGrant']
+        self.progress_to_complete: int = data['progressToComplete']
+        self._activation_date_iso: str = data['activationDate']
+        self._expiration_date_iso: str = data['expirationDate']
+        self.tags: Optional[List[str]] = data['tags']
+        self._objectives: List[ObjectivePayload] = data['objectives']
+        self.asset_path: str = data['assetPath']
+        self._display_name_localized: Localization = Localization(self._display_name, locale=self._state.locale)
+        self._title_localized: Localization = Localization(self._title, locale=self._state.locale)
+
+    def __str__(self) -> str:
+        return self.title.locale
+
+    def __repr__(self) -> str:
+        return f'<Mission title={self.title!r}>'
+
+    def __int__(self) -> int:
+        return self.xp
+
+    def display_name_localized(self, locale: Optional[Union[Locale, str]] = None) -> str:
+        return self._display_name_localized.from_locale(locale)
+
+    def title_localized(self, locale: Optional[Union[Locale, str]] = None) -> str:
+        return self._title_localized.from_locale(locale)
+
+    @property
+    def display_name(self) -> Localization:
+        """:class: `str` Returns the mission's name."""
+        return self._display_name_localized
+
+    @property
+    def title(self) -> Localization:
+        """:class: `str` Returns the mission's title."""
+        return self._title_localized
+
+    @property
+    def type(self) -> Optional[MissionType]:
+        """Optional[:class: `MissionType`] Returns the mission's type."""
+        if self._type == '' or self._type is None:
+            return None
+        return try_enum(MissionType, utils.removeprefix(self._type, 'EAresMissionType::'))
+
+    @property
+    def activation_date(self) -> Optional[datetime.datetime]:
+        """:class: `datetime.datetime` Returns the mission's activation date."""
+        return utils.parse_iso_datetime(self._activation_date_iso) if self._activation_date_iso else None
+
+    @property
+    def expiration_date(self) -> Optional[datetime.datetime]:
+        """:class: `datetime.datetime` Returns the mission's expiration date."""
+        return utils.parse_iso_datetime(self._expiration_date_iso) if self._expiration_date_iso else None
+
+    # @classmethod
+    # def _from_uuid(cls, client: Client, uuid: str) -> Optional[Self]:
+    #     """Returns the mission with the given UUID."""
+    #     data = client._assets.get_mission(uuid)
+    #     return cls(client=client, data=data) if data else None
