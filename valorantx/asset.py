a181b0e45c7fad5cda1163b53f80a141bd288a9f
STACIA <69411527+staciax@users.noreply.github.com>
2023-08-02 14:09:00 +0700

    add asset

---
+++ valorantx/asset.py
@@ -0,0 +1,23 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, Optional
+
+from valorantx.valorant_api.asset import Asset as _Asset
+
+if TYPE_CHECKING:
+    from typing_extensions import Self
+
+    from .client import Client
+
+
+class Asset(_Asset):
+    _state: Client
+
+    if TYPE_CHECKING:
+
+        def __init__(self, state: Client, url: str, *, animated: bool = False, video: bool = False) -> None:
+            ...
+
+        @classmethod
+        def _from_url(cls, state: Client, url: Optional[str] = None, *, animated: bool = False) -> Self:
+            ...
